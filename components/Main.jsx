bbab5dbf337afd43443e67e1b8f32cfdfe8adcf3
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-02-26 01:50:59 +0300

    bg added and styled

---
+++ components/Main.jsx
@@ -0,0 +1,39 @@
+  import React from 'react';
+  import { AiOutlineMail } from 'react-icons/ai';
+  import { FaBlog, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+  const Main = () => {
+    return (
+      <div className="w-full h-screen text-center">
+        <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+          <div>
+            <p className="uppercase text-sm tracking-widest text-gray-600 ">
+              EAT <span className="text-teal-400">BUILD</span> SLEEP
+            </p>
+            <h1 className='py-4 sm:text-md text-white font-semibold'>
+              Welcome to platform that I introduce myself and share my knowledge
+            </h1>
+            <h2 className='py-2 text-white text-xl font-semibold'>
+              Izzet Furkan <span className="text-teal-200">Cakmak</span>
+            </h2>
+            <div className='flex items-center justify-between max-w-[250px] m-auto py-4 lg:max-w-[300px] mb-40'> 
+            <div className='rounded-full bg-black shadow-lg shadow-gray-700 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <FaLinkedinIn/>
+            </div>
+            <div className='rounded-full bg-black shadow-lg shadow-gray-700 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <AiOutlineMail/>
+            </div>
+            <div className='rounded-full bg-black shadow-lg shadow-gray-700 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <FaGithub/>
+            </div>
+            <div className='rounded-full bg-black shadow-lg shadow-gray-700 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <FaBlog/>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  export default Main;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'back':"url('/video.gif')",
+        
+      },
+    },
   },
   plugins: [],
 }
