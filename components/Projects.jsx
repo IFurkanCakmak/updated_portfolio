9690ba20d0105c9293a86899666c09f0af676771
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-03-05 03:00:16 +0300

    project section added

---
+++ components/Projects.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import estatetImg from "../public/projects/realestate.PNG";
+import SingleProject from "./SingleProject";
+
+const Projects = () => {
+  return (
+    <div className="w-full py-12 backdrop-blur-md bg-black/60">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-2xl text-teal-400 tracking-widest'">Projects</p>
+        <p className="py-2 text-3xl lg:text-4xl">What I have buit to learn</p>
+        <div className="grid md:grid-cols-2 gap-8">
+         <SingleProject 
+         title='Real Estate Searcher'
+         backgroundImg={estatetImg}
+         projectUrl='/estate'
+         description='Next JS'
+         />
+         <SingleProject 
+         title='Real Estate Searcher'
+         backgroundImg={estatetImg}
+         projectUrl='/estate'
+         description='Next JS'
+         />
+         <SingleProject 
+         title='Real Estate Searcher'
+         backgroundImg={estatetImg}
+         projectUrl='/estate'
+         description='Next JS'
+         />
+         <SingleProject 
+         title='Real Estate Searcher'
+         backgroundImg={estatetImg}
+         projectUrl='/estate'
+         description='Next JS'
+         />
+        </div>
+        </div>
+      </div>
+    
+  );
+};
+
+export default Projects;
