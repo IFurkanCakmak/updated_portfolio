7920dca98b3b4e0a75fe9a3d057cbb9236ca8a19
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-03-05 03:00:03 +0300

    syntax fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -112,7 +112,7 @@ const Navbar = () => {
       {/* sidebar */}   
     <div className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-white/10" :''}>
     <div className=
-    {nav ? "fixed left-0 top-0 w-[75%] md:w-[60%] md:w-[45%] h-screen bg-black p-10 ease-in duration-500"
+    {nav ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-black p-10 ease-in duration-500"
     : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
     <div>
       <div className="flex w-full items-center justify-between">
