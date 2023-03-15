5b32876d90dd29d121db635a3e40d3b4624dc521
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-03-15 03:11:32 +0300

    added

--- pages/estate.jsx
+++ pages/estate.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Image from 'next/image'
+import estateImg from "../public/projects/realestate.PNG";
+import { RiRadioButtonFill } from 'react-icons/ri';
+import Link from 'next/link';
 
 const estate = () => {
   return (
-    <div>estate</div>
+    <div className='w-full'>
+    <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+      <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
+      <Image
+        className='absolute z-1'
+        layout='fill'
+        objectFit='cover'
+        src={estateImg}
+        alt='/'
+      />
+    </div>
+    </div>
+ 
+      
+
+    
+    
   )
 }
+
 
 export default estate
