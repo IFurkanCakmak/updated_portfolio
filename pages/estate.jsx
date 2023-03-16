e75d6684b34cd37da9045a069471e361e65c362b
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-03-16 04:12:42 +0300

    project page route added

--- pages/estate.jsx
+++ pages/estate.jsx
@@ -1,30 +1,59 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 import estateImg from "../public/projects/realestate.PNG";
-import { RiRadioButtonFill } from 'react-icons/ri';
-import Link from 'next/link';
+import { RiRadioButtonFill } from "react-icons/ri";
+
 
 const estate = () => {
   return (
-    <div className='w-full'>
-    <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
-      <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
-      <Image
-        className='absolute z-1'
-        layout='fill'
-        objectFit='cover'
-        src={estateImg}
-        alt='/'
-      />
+    <div className="w-full lg:h-screen md:h-screen bg-black/90 backdrop-blur-lg">
+      <div className="w-screen h-[30vh] lg:h-[40vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/90 z-10" />
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={estateImg}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <p className="py-2 text-2xl lg:text-5xl font-semibold"> Real Estate Web App</p>
+          <p className="text-lg lg:text-2xl">Next.js / Axios / Chakra UI</p>
+        </div>
+      </div>
+
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
+        <div className="col-span-4">
+          <p className="text-xl lg:text-2xl text-teal-400 tracking-widest'">PROJECT</p>
+          <p className="py-2 mb-1 text-xl lg:text-4xl">Overview</p>
+          <p>
+            I specialize in building mobile responsive front-end UI applications
+            that connect with API’s and other backend technologies. I’m
+            passionate about learning new technologies and understand there is
+            more than one way to accomplish a task. Though I am most proficient
+            in building front-end applications using HTML, CSS, Javascript, and
+            React, I am a quick learner and can pick up new tech stacks as
+            needed. I believe that being a great developer is not using one
+            specific language, but choosing the best tool for the job.
+          </p>
+          <button className="px-8 py-2 mt-5 shadow-sm mr-6">Live</button>
+          <button className="px-8 py-2 mt-5 shadow-sm">Code</button>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-md shadow-gray-800 rounded-xl p-4">
+        <div className="p-2">
+          <p className="text-center font-bold pb-2">Technologies</p>
+          <div className="grid grid-cols-3 md:grid-cols-1">
+            <p className="py-2 flex items-center font-semibold"><RiRadioButtonFill className="mt-1 pr-1 text-teal-600"/>Next</p>
+            <p className="py-2 flex items-center font-semibold"><RiRadioButtonFill className="mt-1 pr-1 text-teal-600"/>Next</p>
+            <p className="py-2 flex items-center font-semibold"><RiRadioButtonFill className="mt-1 pr-1 text-teal-600"/>Next</p>
+            <p className="py-2 flex items-center font-semibold"><RiRadioButtonFill className="mt-1 pr-1 text-teal-600"/>Next</p>
+            <p className="py-2 flex items-center font-semibold"><RiRadioButtonFill className="mt-1 pr-1 text-teal-600"/>Next</p>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
-    </div>
- 
-      
+  );
+};
 
-    
-    
-  )
-}
-
-
-export default estate
+export default estate;
