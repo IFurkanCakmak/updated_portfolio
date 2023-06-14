3446c58e6924ae309f587eefa91badf53075d467
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-06-14 21:21:40 +0300

    tawkto added

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,26 @@
 import '@/styles/globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
+import { useRef } from 'react';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
+  
+  const tawkMessengerRef = useRef();
+
+  const handleMinimize = () => {
+      tawkMessengerRef.current.minimize();
+  };
+
+  return (   
+    <>    
     <Navbar/>
-   
+
       <Component {...pageProps} />
+      <button onClick={handleMinimize}> Minimize the Chat </button>
+      <TawkMessengerReact
+                propertyId="63f1c0224247f20fefe16000"
+                widgetId="1gpk5d29r"/>
       <Footer/>
     </>
   )
