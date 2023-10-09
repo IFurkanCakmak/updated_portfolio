680e158e6a13082915d1e0f958e21d992083c8f7
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-10-10 01:35:19 +0300

    fix

--- components/About.jsx
+++ components/About.jsx
@@ -9,24 +9,28 @@ const About = () => {
     >
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8 mt-40">
         <div className="col-span-2">
-          <p className="text-2xl text-teal-400 tracking-widest">About</p>
-          <p className="py-2 text-4xl">Who I am</p>
-          <p className="py-1 text-gray-400">
+          <p className="text-2xl text-teal-400 tracking-widest p-8 sm:p-0 md:p-0 lg:p-0 xl:p-0">
+            About
+          </p>
+          <p className="text-4xl sm:py-2 md:py-2 lg:py-2 xl:py-2 px-8 sm:p-0 md:p-0 lg:p-0 xl:p-0">
+            Who I am
+          </p>
+          <p className=" sm:py-1 md:py-1 lg:py-1 xl:py-1 px-8 sm:p-0 md:p-0 lg:p-0 xl:p-0 text-gray-400">
             I am developer with engineering principles
           </p>
-          <p className="py-2">
-            I am about graduate from Duzce University Computer Engineering
-            department and now i specialize in building mobile responsive front-end UI
-            applications that connect with APIs and other backend technologies.
-            I am passionate about learning new technologies and understand there
-            is more than one way to accomplish a task. Though I am most
-            proficient in building front-end applications using HTML, CSS,
-            Javascript, and React and its frameworks such as Next.js, I am a
-            really fast learner and can pick up new tech stacks as needed. I
+          <p className="sm:py-2 md:py-2 lg:py-2 xl:py-2 p-8 sm:p-0 md:p-0 lg:p-0 xl:p-0">
+            I am fresh graduate from Duzce University Computer Engineering
+            department and now i specialize in building mobile responsive
+            front-end UI applications that connect with APIs and other backend
+            technologies. I am passionate about learning new technologies and
+            understand there is more than one way to accomplish a task. Though I
+            am most proficient in building front-end applications using HTML,
+            CSS, Javascript, and React and its frameworks such as Next.js, I am
+            a really fast learner and can pick up new tech stacks as needed. I
             believe that being a great developer is not using one specific
             language, but choosing the best tool for the job.
           </p>
-          <p className="py-2 mb-6">
+          <p className="sm:py-2 md:py-2 lg:py-2 xl:py-2 p-8 sm:p-0 md:p-0 lg:p-0 xl:p-0 mb-6">
             Besides, i am not the one who thinks it is a good idea to stuck at
             just one spesific technology to build something. There is million
             way to create a complete application which makes life easier for the
@@ -37,7 +41,7 @@ const About = () => {
             will can.
           </p>
         </div>
-        <div className="w-full bg-black h-auto m-auto shadow-xl shadow-gray-800 rounded-xl flex items-center justify-center p-3 hover:scale-105 ease-in duration-300">
+        <div className="w-full bg-black h-auto m-auto shadow-lg shadow-gray-800 rounded-xl flex items-center justify-center sm:p-3 lg:p-3 md:p-3 p-6 hover:scale-105 ease-in duration-300">
           <Image
             className="rounded-xl"
             src="/about-photo.jpg"
