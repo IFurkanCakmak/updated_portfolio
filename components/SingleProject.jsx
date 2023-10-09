6666526d6432c5b8255dee24c1a59bcf2a91fb18
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-10-10 01:36:21 +0300

    update

--- components/SingleProject.jsx
+++ components/SingleProject.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-
-const SingleProject = ({title, backgroundImg, projectUrl, description}) => {
+const SingleProject = ({ title, backgroundImg, projectUrl, description }) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full mt-5 shadow-xl shadow-gray-800 rounded-xl p-4 group hover:bg-gradient-to-r from-teal-400 to-teal-600  ">
+    <div className="relative flex items-center justify-center h-auto w-full mt-5 shadow-lg shadow-gray-800 rounded-xl p-4 group hover:bg-gradient-to-r from-teal-400 to-teal-600  ">
       <Image
         className="rounded-xl group-hover:opacity-20"
         src={backgroundImg}
@@ -15,7 +14,9 @@ const SingleProject = ({title, backgroundImg, projectUrl, description}) => {
         <p className="text-2xl text-center font-bold text-white tracking-wider">
           {title}
         </p>
-        <p className="pb-4 pt-2 text-lg font-semibold text-center text-white">{description}</p>
+        <p className="pb-4 pt-2 text-lg font-semibold text-center text-white">
+          {description}
+        </p>
         <Link href={projectUrl}>
           <p className="text-center py-3 rounded-lg bg-white text-gray-600 font-bold text-lg cursor-pointer">
             See Project
